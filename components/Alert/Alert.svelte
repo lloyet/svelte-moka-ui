a8c32596f9156eb53b51b77134f7d8b61e95cd67
lloyet <contact@lloyet.dev>
2020-12-18 12:54:42 +0100

    feat(all): first push

---
+++ components/Alert/Alert.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { fly } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	import Icon from 'mdi-svelte';
+	import { mdiClose } from '@mdi/js';
+	
+	let klass = '';
+	export { klass as class };
+	export let dense: boolean = false;
+	export let outlined: boolean = false;
+	export let rounded: boolean = false;
+	export let color: string = 'limegreen';
+	export let border: string = 'left';
+	export let dismissible: boolean = false;
+	export let visible: boolean = false;
+	export let style: string = '';
+	export let transition: any = fly;
+	export let transitionOpts: any = { x: 100, duration: 800, easing: quintOut };
+	
+	const handleShow = (): void => {
+		visible = false;
+	};
+	
+	onMount((): void => {
+		visible = true;
+	});
+</script>
+
+<style lang="scss" src="./Alert.scss"></style>
+
+{#if visible}
+	<div
+	role="alert"
+	class="c-alert {klass}"
+	class:dense
+	class:border
+	class:outlined
+	style="--padding: {dense ? '5px' : '25px'}; --radius: {rounded ? '5px' : '0'}; --shadow: {outlined ? '0 0 0px 1px #696969' : 'none'}; --color: {color}; border-{border}: 5px solid {color}; {style}"
+	transition:transition="{transitionOpts}">
+		<div class="icon">
+			<slot name="icon" />
+		</div>
+		<div class="content">
+			<slot name="content"/>
+		</div>
+		{#if dismissible}
+			<div class="closer">
+				<button class="closer" on:click="{handleShow}">
+					<Icon path="{mdiClose}" color="#696969"/>
+				</button>
+			</div>
+		{/if}
+	</div>
+{/if}
---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import Icon from 'mdi-svelte';
+	import { mdiFire } from '@mdi/js';
+	import Alert from '../components/Alert/Alert.svelte';
+
+	export let name: string;
+	let visible: boolean;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<Alert bind:visible color="#2ff5a5" rounded dismissible>
+		<div slot="icon" style="display: flex; align-items: center; justify-content: center;">
+			<Icon path="{mdiFire}"/>
+		</div>
+		<div style="height: 100%; display: flex; justify-content: center; align-items: center;" slot="content">Visit the <a href="https://svelte.dev/tutorial">Svelte tutooooooooorial</a> to learn how to build</div>
+	</Alert>
+	<button on:click="{() => { visible = true }}">reset</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		background: lightgrey;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
